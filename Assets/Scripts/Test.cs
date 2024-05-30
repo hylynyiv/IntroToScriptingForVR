f5fffccf86cd18153aaa44d93efa858948f9fc65
hylynyiv <info@hylynyiv.com>
2024-05-30 17:24:37 +0200

    Adding FrameRateLimiter

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    int count;
     string petName = "Garfield";
     float weight = 13.5f;
     float weightGain = 0.5f;
-    int age = 8;
-    float agingRate = 1f / 30 / 60; // we want Garfield to age 1 year every 30 
-    // seconds. With a frame rate of 60 Hz, which is 60 frames per second, we 
-    // can calculate the percentage of aging per frame by dividing 1/30/60 or 
+    float age = 8f;
+    float agingRate = 1f/30/60;
+    // We want Garfield to age 1 year every 30 seconds. 
+    // With a frame rate of 60 Hz, which is 60 frames per second, we can
+    // calculate the percentage of aging per frame by dividing 1/30/60 or 
     // 1/(30*60).
 
-    float newAge = 8;
 
     void Start()
     {
@@ -23,8 +24,10 @@ public class Test : MonoBehaviour
 
     void Update()
     {
-        newAge += agingRate;
-        Debug.Log($"{petName}'s age is now {newAge}.\n");
+        count += 1;
+        Debug.Log($"{count}");
+        age += agingRate;
+        Debug.Log($"{petName}'s age is now {age}.\n");
     }
 }
 
