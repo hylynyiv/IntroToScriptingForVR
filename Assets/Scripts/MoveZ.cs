c41cf82a292d48aa40651b226c526655af80ffb8
hylynyiv <info@hylynyiv.com>
2024-05-30 18:24:35 +0200

    Adding move script

---
+++ Assets/Scripts/MoveZ.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveZ : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.1f * Time.deltaTime);
+    }
+}
+
