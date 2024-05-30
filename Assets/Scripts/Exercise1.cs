18ca3964694592764daacd1ce41ef0af60f0a474
hylynyiv <info@hylynyiv.com>
2024-05-30 22:44:08 +0200

    Adding Exercise1

---
+++ Assets/Scripts/Exercise1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercise1 : MonoBehaviour
+{
+    public string petName = "Garfield";
+    public float weight = 13.5f;
+    public float speed = 0.1f;
+    public float hunger = 0;
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+        hunger += 0.1f;
+        hunger += speed * Time.deltaTime * 0.1f;
+        weight -= speed * Time.deltaTime * 0.1f;
+    }
+}
+
