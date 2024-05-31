1e51cffb14c1e4651cce1a42d0a485a76540d829
hylynyiv <info@hylynyiv.com>
2024-05-31 15:10:27 +0200

    Added random movement Z

---
+++ Assets/Scripts/MoveRandom.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRandom : MonoBehaviour
+{
+    public float speed = 1f;
+    public float minSpeed = -3f;
+    public float maxSpeed = 3f;
+    public float changeInterval = 2.0f;
+    public float timer;
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= changeInterval)
+        {
+            timer = 0;
+            speed = Random.Range(minSpeed, maxSpeed);
+        }
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + speed * Time.deltaTime);
+    }
+}
+
+
