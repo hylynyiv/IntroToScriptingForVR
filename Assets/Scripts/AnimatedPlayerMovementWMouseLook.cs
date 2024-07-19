034e226d540824072aeb8418e5807258e53adfc5
hylynyiv <info@hylynyiv.com>
2024-07-19 02:20:23 +0200

    Fixing Error

--- Assets/Scripts/AnimatedPlayerMovementWMouseLook.cs
+++ Assets/Scripts/AnimatedPlayerMovementWMouseLook.cs
@@ -1,19 +1,17 @@
-// Attach this script to a player object. the camera should be a child of it.
-// In the Inspector of the Player object, drag the camera into the "Player
-// Camera" field of the script.
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimatedPlayerMovementWMouseLook : MonoBehaviour
-
 {
-    public float speed = 7.0f;
-    public float mouseSensitivity = 300.0f;
-    public Transform playerCamera;
+    [Header("Mouse Look Settings")]
+    public float mouseSensitivity = 100f;
+    public Transform cameraTransform;
 
-    private float xRotation = 0.0f;
+    [Header("Movement Settings")]
+    public float speed = 12f;
+    public CharacterController controller;
+    public Animator animator;
+
+    private float xRotation = 0f;
 
     void Start()
     {
@@ -22,35 +20,38 @@ public class AnimatedPlayerMovementWMouseLook : MonoBehaviour
 
     void Update()
     {
-        // Mouse look
+        HandleMouseLook();
+        HandleMovement();
+    }
+
+    void HandleMouseLook()
+    {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
+    }
 
-        // WASD movement
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    void HandleMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        Vector3 move = transform.right * x + transform.forward * z;
 
+        controller.Move(move * speed * Time.deltaTime);
 
-        if (movement != 0 )
+        if (move != Vector3.zero)
         {
-            animator.SetBool("isWalking", true);
+            animator.SetBool("isMoving", true);
         }
         else
         {
-            animator.SetBool("isWalking", false);
+            animator.SetBool("isMoving", false);
         }
-
-        // You can also use this more concise notation:
-        //bool isWalking = move != 0 || strafe != 0;
-        //animator.SetBool("isWalking", isWalking);
     }
 }
