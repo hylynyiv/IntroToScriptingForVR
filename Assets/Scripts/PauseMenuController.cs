410bc9d667d82536a08e82aa27e2787b91cd2b2e
hylynyiv <info@hylynyiv.com>
2024-07-22 14:19:08 +0200

    Adding PauseMenuController

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        // Ensure the pause menu is not visible at the start
+        pauseMenuUI.SetActive(false);
+        // Lock the cursor at the start
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenuUI.activeSelf)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
